0e35501e92bdc9ebe42132de908d27476737eeb0
Dzianis Perahudau <tvigger@yandex.ru>
2023-11-14 21:50:41 +0300

    feat: add solution for toFixed function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -405,8 +405,12 @@ function toExponential(/* number, fractionDigits */) {
  * 12345, 2    => '12345.00'
  * 12.345, 1   => '12.3'
  */
-function toFixed(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toFixed(number, fractionDigits) {
+  const numStr = number.toString();
+  if (Number.isInteger(number) === true) {
+    return number.toFixed(fractionDigits);
+  }
+  return numStr.slice(0, numStr.indexOf('.') + fractionDigits + 1);
 }
 
 /**
